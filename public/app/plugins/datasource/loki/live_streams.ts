a03fea219e9dc6fdd6ad76889c737bf036efe301
Ivana <ivana.huckova@gmail.com>
2020-11-27 17:22:11 +0100

    Refactor, throw error only ono abnormal closures

--- public/app/plugins/datasource/loki/live_streams.ts
+++ public/app/plugins/datasource/loki/live_streams.ts
@@ -42,18 +42,17 @@ export class LiveStreams {
         appendResponseToBufferedData(response, data);
         return [data];
       }),
+
       retryWhen((attempts: Observable<any>) =>
         attempts.pipe(
-          mergeMap((error, i) => {
-            const retryAttempt = i + 1;
-            // If maximum number of retries have been met, throw error
-            if (retryAttempt > 3) {
-              return throwError(`error: ${error.reason}`);
+          mergeMap(error => {
+            // Code 1006 is used to indicate that a connection was closed abnormally.
+            // If connection was closed abnormally, we wish to retry, otherwise throw error.
+            if (error.code === 1006) {
+              console.log('Reconnecting websocket...');
+              return timer(1000);
             }
-
-            // Retry to reconnect after 1s
-            console.log('Reconnecting websocket...');
-            return timer(1000);
+            return throwError(`error: ${error.reason}`);
           })
         )
       ),
@@ -61,8 +60,34 @@ export class LiveStreams {
         delete this.streams[target.url];
       })
     );
+
     this.streams[target.url] = stream;
 
     return stream;
   }
 }
+
+// export const genericRetryStrategy = ({
+//   maxRetryAttempts = 3,
+//   scalingDuration = 1000,
+//   excludedStatusCodes = [],
+// }: {
+//   maxRetryAttempts?: number;
+//   scalingDuration?: number;
+//   excludedStatusCodes?: number[];
+// } = {}) => (attempts: Observable<any>) => {
+//   return attempts.pipe(
+//     mergeMap((error, i) => {
+//       const retryAttempt = i + 1;
+//       // if maximum number of retries have been met
+//       // or response is a status code we don't wish to retry, throw error
+//       if (retryAttempt > maxRetryAttempts || excludedStatusCodes.find(e => e === error.status)) {
+//         return throwError(error);
+//       }
+//       console.log(`Attempt ${retryAttempt}: retrying in ${retryAttempt * scalingDuration}ms`);
+//       // retry after 1s, 2s, etc...
+//       return timer(retryAttempt * scalingDuration);
+//     }),
+//     finalize(() => console.log('We are done!'))
+//   );
+// };
