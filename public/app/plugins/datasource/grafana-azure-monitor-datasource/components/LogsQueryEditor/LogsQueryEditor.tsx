462cdcac763716f615b12bde5c6dcc491d363eab
Sarah Zinger <sarah.zinger@grafana.com>
2021-05-14 17:11:54 -0400

    WIP:Begin thinking about how to make queries with scope determined by the resource picker

--- public/app/plugins/datasource/grafana-azure-monitor-datasource/components/LogsQueryEditor/LogsQueryEditor.tsx
+++ public/app/plugins/datasource/grafana-azure-monitor-datasource/components/LogsQueryEditor/LogsQueryEditor.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { AzureMonitorErrorish, AzureMonitorOption, AzureMonitorQuery } from '../../types';
 import Datasource from '../../datasource';
-import { InlineFieldRow, Modal } from '@grafana/ui';
-import SubscriptionField from '../SubscriptionField';
-import WorkspaceField from './WorkspaceField';
+import { Modal } from '@grafana/ui';
+
 import QueryField from './QueryField';
 import FormatAsField from './FormatAsField';
 import ResourcePicker from '../ResourcePicker';
+import { Row, RowGroup } from '../ResourcePicker/types';
 
 interface LogsQueryEditorProps {
   query: AzureMonitorQuery;
@@ -25,30 +25,42 @@ const LogsQueryEditor: React.FC<LogsQueryEditorProps> = ({
   onChange,
   setError,
 }) => {
+  // TODO: handle saved queries and translating that scope into a selected resource.
+  // TODO: how should we handle opening empty folders?
+  const [selectedResource, setSelectedResource] = useState<RowGroup>({});
+  const handleSelectResource = useCallback(
+    (row: Row, isSelected: boolean) => {
+      if (isSelected) {
+        setSelectedResource({ [row.id]: row });
+        onChange({
+          ...query,
+          subscription: row.subscriptionId,
+          azureLogAnalytics: {
+            ...query.azureLogAnalytics,
+            workspace: row.name, // TODO: not sure what to put here? Josh mentioned something about a resource uri?
+          },
+        });
+      } else {
+        setSelectedResource({});
+      }
+    },
+    [onChange, setSelectedResource, query]
+  );
+
+  const [isResourcePickerOpen, setIsResourcePickerOpen] = useState(false);
+
   return (
     <div data-testid="azure-monitor-logs-query-editor">
-      <Modal title="Select a resource" isOpen={true}>
-        <ResourcePicker resourcePickerData={datasource.resourcePickerData} />
+      <Modal title="Select a resource" isOpen={isResourcePickerOpen} onDismiss={() => setIsResourcePickerOpen(false)}>
+        <ResourcePicker
+          resourcePickerData={datasource.resourcePickerData}
+          selectedResource={selectedResource}
+          handleSelectResource={handleSelectResource}
+        />
       </Modal>
 
-      <InlineFieldRow>
-        <SubscriptionField
-          query={query}
-          datasource={datasource}
-          subscriptionId={subscriptionId}
-          variableOptionGroup={variableOptionGroup}
-          onQueryChange={onChange}
-          setError={setError}
-        />
-        <WorkspaceField
-          query={query}
-          datasource={datasource}
-          subscriptionId={subscriptionId}
-          variableOptionGroup={variableOptionGroup}
-          onQueryChange={onChange}
-          setError={setError}
-        />
-      </InlineFieldRow>
+      {/* TODO use a real component here, some kind of breadcrumb like button */}
+      <button onClick={() => setIsResourcePickerOpen(true)}>Change Scope</button>
 
       <QueryField
         query={query}
