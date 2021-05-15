1f8dab93ea4f078f81ca0ed31e36c4d5bdf2aa2d
Ganesh Vernekar <ganeshvern@gmail.com>
2021-05-15 14:01:35 +0530

    NGAlert: Add GoogleChat notification channel
    Signed-off-by: Ganesh Vernekar <ganeshvern@gmail.com>

--- pkg/services/ngalert/notifier/alertmanager.go
+++ pkg/services/ngalert/notifier/alertmanager.go
@@ -420,6 +420,8 @@ func (am *Alertmanager) buildReceiverIntegrations(receiver *apimodels.PostableAp
 			n, err = channels.NewDingDingNotifier(cfg, tmpl)
 		case "webhook":
 			n, err = channels.NewWebHookNotifier(cfg, tmpl)
+		case "googlechat":
+			n, err = channels.NewGoogleChatNotifier(cfg, tmpl)
 		default:
 			return nil, fmt.Errorf("notifier %s is not supported", r.Type)
 		}
